65c60779991f53a2352cdfd13204d7485b68772e
ashbbdu <ashishsrivastava7007@gmail.com>
2025-10-22 23:58:23 +0530

    hashing questions

--- Basics/src/Hashing/BasicHashing.java
+++ Basics/src/Hashing/BasicHashing.java
@@ -2,6 +2,7 @@ package Hashing;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class BasicHashing {
     public static boolean checkDuplicate(int [] nums) {
@@ -12,16 +13,67 @@ public class BasicHashing {
         }
         return false;
     }
+
+    public static void countFrequencies (int [] nums) {
+        HashMap<Integer , Integer> freq = new HashMap<>();
+        for(var num : nums) {
+            if(freq.containsKey(num)) {
+                int currentCount = freq.get(num);
+                freq.put(num , currentCount + 1);
+            } else {
+                freq.put(num , 1);
+            }
+        }
+
+        System.out.println(freq);
+    }
+
+
+    public static int countHighestFrequencies (int [] nums) {
+        HashMap<Integer , Integer> freq = new HashMap<>();
+
+        for(var num : nums) {
+            if(freq.containsKey(num)) {
+                int currentCount = freq.get(num);
+                freq.put(num , currentCount + 1);
+            } else {
+                freq.put(num , 1);
+            }
+        }
+        int highest = -1;
+        int numberWithHighestFreq = -1;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+            System.out.println(key + " -> " + value);
+//       try doing dry run
+            if (highest < value) {
+                highest = value;
+                numberWithHighestFreq = key;
+            } else if(value == highest) {
+                numberWithHighestFreq = Math.min(numberWithHighestFreq, key);
+            }
+
+        }
+
+
+    return numberWithHighestFreq;
+    }
     public static void main(String[] args) {
         HashMap<String , Integer>  map = new HashMap<>();
         map.put("Ashish" , 1);
         map.put("Virat" , 18);
         map.put("Rohit" , 45);
         System.out.println(map.containsKey("Ashish"));
+        System.out.println(map.keySet());
 
 //        Check if the array container duplicate
         int[] nums = {1, 2, 3, 1};
         boolean ans = checkDuplicate(nums);
         System.out.println(ans);
+        int [] nums1 = {100,200,200,300,300};
+        countFrequencies(nums1);
+        System.out.println( countHighestFrequencies(nums1));;
     }
 }
