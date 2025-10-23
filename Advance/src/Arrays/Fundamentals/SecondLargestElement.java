abda8cc97b6b761b72797d0ff5e38922c1ef2fbf
ashbbdu <ashishsrivastava7007@gmail.com>
2025-10-23 23:26:05 +0530

    secondLargestBetter

--- Advance/src/Arrays/Fundamentals/SecondLargestElement.java
+++ Advance/src/Arrays/Fundamentals/SecondLargestElement.java
@@ -14,8 +14,29 @@ public class SecondLargestElement {
         return -1;
     }
 
+
+    public static int secondLargestBetter (int [] nums) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(nums[i] > largest) {
+                largest = nums[i];
+            }
+        }
+
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(nums[i] > secondLargest && nums[i] != largest ) {
+                secondLargest = nums[i];
+            }
+        }
+
+        return secondLargest == Integer.MIN_VALUE ? -1 : secondLargest;
+    }
+
     public static void main(String[] args) {
-        int [] arr = {7, 7, 2, 2, 10, 10, 10};
+        int [] arr = {8 , 8 , 8, 8, 8};
         System.out.println(secondLargestBrute(arr));;
+        System.out.println(secondLargestBetter(arr));;
     }
 }
