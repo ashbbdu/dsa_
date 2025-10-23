05ecf3d9425d01c94b061df3db665869ddde4d96
ashbbdu <ashishsrivastava7007@gmail.com>
2025-10-23 12:44:38 +0530

    started oops

--- Basics/src/Language/IntroOOPs.java
+++ Basics/src/Language/IntroOOPs.java
@@ -1,7 +1,28 @@
 package Language;
 
+class Employee {
+    public String name;
+    public int salary;
+
+
+//    Constructor
+    public Employee (String _name , int _salary) {
+       name = _name;
+       salary = _salary;
+    }
+
+   public  String getName () {
+        return name;
+    }
+
+
+
+}
+
 public class IntroOOPs {
     public static void main(String[] args) {
-
+        Employee e1 = new Employee("Ashish" , 1000000000);
+        e1.name = "ash";
+        System.out.println(e1.getName());
     }
 }
