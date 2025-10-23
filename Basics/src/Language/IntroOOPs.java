e3ba68bfd8df81a20d3de65001df01cf20006451
ashbbdu <ashishsrivastava7007@gmail.com>
2025-10-23 17:06:14 +0530

    student class

--- Basics/src/Language/IntroOOPs.java
+++ Basics/src/Language/IntroOOPs.java
@@ -19,10 +19,35 @@ class Employee {
 
 }
 
+class Student {
+    public String name;
+    public int rollNumber;
+
+//    public Student (String _name , int _rollNumber) {
+//        this.name = _name;
+//        this.rollNumber = _rollNumber;
+//    }
+
+    public void setDetails (String _name , int _rollNumber) {
+        this.name = _name;
+        this.rollNumber = _rollNumber;
+    }
+
+    public void displayDetails () {
+        System.out.println("Name : " + this.name);
+        System.out.println("Roll Number : " + this.rollNumber);
+    }
+}
+
 public class IntroOOPs {
     public static void main(String[] args) {
         Employee e1 = new Employee("Ashish" , 1000000000);
         e1.name = "ash";
         System.out.println(e1.getName());
+
+        Student student = new Student();
+        student.name = "ashish";
+        student.rollNumber = 21;
+        student.displayDetails();
     }
 }
