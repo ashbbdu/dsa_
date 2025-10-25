5f9f94983a270780dc4405841653f9bc58464b5c
ashbbdu <ashishsrivastava7007@gmail.com>
2025-10-26 00:13:30 +0530

    removeDuplicatesBrute

--- Advance/src/Arrays/LoginBuidling/RemoveDuplicatesFromASortedArray.java
+++ Advance/src/Arrays/LoginBuidling/RemoveDuplicatesFromASortedArray.java
@@ -1,10 +1,23 @@
 package Arrays.LoginBuidling;
 
-public class RemoveDuplicatesFromASortedArray {
-    public int removeDuplicates(int[] nums) {
+import java.util.TreeSet;
 
+public class RemoveDuplicatesFromASortedArray {
+    public static int removeDuplicatesBrute(int[] nums) {
+        TreeSet<Integer> ts = new TreeSet<>();
+        int cnt = 0;
+        for(var num : nums) {
+            ts.add(num);
+        }
+
+        for(var num : ts) {
+            nums[cnt++] = num;
+        }
+
+        return ts.size();
     }
     public static void main(String[] args) {
-
+        int [] nums = {0, 0, 3, 3, 5, 6};
+        System.out.println(removeDuplicatesBrute(nums));
     }
 }
