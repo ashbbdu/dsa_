8befd4266cb78e1fde610de34265d90bc794926a
ashbbdu <ashishsrivastava7007@gmail.com>
2025-10-26 00:31:06 +0530

    removeDuplicatesOptimal

--- Advance/src/Arrays/LoginBuidling/RemoveDuplicatesFromASortedArray.java
+++ Advance/src/Arrays/LoginBuidling/RemoveDuplicatesFromASortedArray.java
@@ -16,8 +16,21 @@ public class RemoveDuplicatesFromASortedArray {
 
         return ts.size();
     }
+
+
+    public static int removeDuplicatesOptimal(int[] nums) {  int i = 0;
+        for(int j = 1; j < nums.length ; j++) {
+            if(nums[i] != nums[j]) {
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
     public static void main(String[] args) {
         int [] nums = {0, 0, 3, 3, 5, 6};
+        int [] arr = {0, 0, 3, 3, 5, 6};
         System.out.println(removeDuplicatesBrute(nums));
+        System.out.println(removeDuplicatesOptimal(arr));
     }
 }
