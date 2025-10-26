344ad17676220e3768d8c23aa3d9b1deb2daff39
ashbbdu <ashishsrivastava7007@gmail.com>
2025-10-26 22:58:29 +0530

    missingNumberBrute

---
+++ Advance/src/Arrays/LoginBuidling/FindMissingNumber.java
@@ -0,0 +1,17 @@
+package Arrays.LoginBuidling;
+
+public class FindMissingNumber {
+    public static int missingNumberBrute(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int sum = (n * (n + 1)) / 2;
+        for(int i = 0 ; i < n ; i++) {
+            count = count + nums[i];
+        }
+        return sum - count;
+    }
+    public static void main(String[] args) {
+        int [] nums = {0, 2, 3, 1, 4};
+        System.out.println(missingNumberBrute(nums));
+    }
+}
