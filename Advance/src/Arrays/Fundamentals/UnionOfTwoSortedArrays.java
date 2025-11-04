bd2e25aadee7f46a5a54e5ceed9df8a1e0c27ea2
ashbbdu <ashishsrivastava7007@gmail.com>
2025-11-04 23:31:26 +0530

    union of two sorted arrays

--- Advance/src/Arrays/Fundamentals/UnionOfTwoSortedArrays.java
+++ Advance/src/Arrays/Fundamentals/UnionOfTwoSortedArrays.java
@@ -1,23 +1,24 @@
 package Arrays.Fundamentals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UnionOfTwoSortedArrays {
 
-    public static List<Integer> unionArrayOptimal (int [] nums1 ,  int [] nums2) {
+    public static int [] unionArrayOptimal (int [] nums1 ,  int [] nums2) {
         int i = 0;
         int j = 0;
         List<Integer> ans = new ArrayList<>();
 
         while (i < nums1.length && j < nums2.length) {
             if(nums1[i] <= nums2[j] ) {
-                if(ans.isEmpty() || ans.get(ans.getLast() - 1) != nums1[i]) {
+                if(ans.isEmpty() || ans.get(ans.size() - 1) != nums1[i]) {
                     ans.add(nums1[i]);
                 }
                 i++;
             } else {
-                if(ans.isEmpty() || ans.get(ans.getLast() - 1) != nums2[j]) {
+                if(ans.isEmpty() || ans.get(ans.size() - 1) != nums2[j]) {
                     ans.add(nums2[j]);
 
                 }
@@ -27,27 +28,31 @@ public class UnionOfTwoSortedArrays {
 
 //        if nums2 is empty;
         while(i < nums1.length) {
-            if(ans.isEmpty() || ans.get(ans.getLast() -1 ) != nums1[i]) {
+            if(ans.isEmpty() || ans.get(ans.size() -1 ) != nums1[i]) {
                 ans.add(nums1[i]);
             }
             i++;
         }
 //      if nums1 is empty;
         while(j < nums2.length) {
-            if(ans.isEmpty() || ans.get(ans.getLast() -1 ) != nums2[j]) {
+            if(ans.isEmpty() || ans.get(ans.size() -1 ) != nums2[j]) { // ans.isEmpty() is only used to haddnle these cases  nums1 = [] , nums2 = [] etc
                 ans.add(nums2[j]);
             }
             j++;
         }
-
-        return ans;
+        int [] ansArray = new int [ans.size()];
+        for(int k = 0 ;  k < ans.size() ; k++) {
+            ansArray[k] = ans.get(k);
+        }
+        return ansArray;
     }
 
     public static void main(String[] args) {
         int [] arr1 = {1 , 1 , 2 , 3 , 4 , 5};
         int [] arr2 = {2 , 3 , 4 , 4 , 5 , 6};
 
-        System.out.println((unionArrayOptimal(arr1 , arr2)));
+        int [] ans = (unionArrayOptimal(arr1 , arr2));
+        System.out.println(Arrays.toString(ans));
 
 
     }
